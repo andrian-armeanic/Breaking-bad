6ace8c667172f9849dbfeacbc8438425f43f317c
andrian-armeanic <andrian.armeanic@feel-it-services.com>
2022-07-08 13:46:08 +0300

    fixed packages

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,31 +1,26 @@
-import { Grid } from "@material-ui/core";
-import { Box, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Box, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import { ChangeEvent, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { search } from "src/reducer/search";
-import { store } from "../store";
+import { Dispatch } from "redux";
+import { search } from "../reducer/search";
 import "./index.scss";
 
-export interface IName {
-    name: number;
-}
+export type IName = { name: number; }
 
-export interface IPortrayed {
-    portrayed: number;
-}
+export type IPortrayed = { portrayed: number; }
 
 export default function App () {
     const [searchValue, setSearchValue] = useState<string>("");
     const [dropDownValue, setDropDownValue] = useState("1");
-    const [userValue, setUserValue] = useState<any[]>([]);
-    const dispatch = useDispatch<typeof store.dispatch>();
+    const [userValue, setUserValue] = useState<object[]>([]);
+    const dispatch = useDispatch<Dispatch>();
     const { user, isError, isSuccess, message } = useSelector((state: any) => state.search);
 
     const characterAscending = (a: IName, b: IName) => {
         return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
     };
 
-    const characterDesscending = (a: IName, b: IName) => {
+    const characterDescending = (a: IName, b: IName) => {
         return a.name < b.name ? 1 : a.name > b.name ? -1 : 0;
     };
 
@@ -33,8 +28,16 @@ export default function App () {
         return a.portrayed < b.portrayed ? -1 : a.portrayed > b.portrayed ? 1 : 0;
     };
 
-    const actorDesscending = (a: IPortrayed, b: IPortrayed) => {
+    const actorDescending = (a: IPortrayed, b: IPortrayed) => {
         return a.portrayed < b.portrayed ? 1 : a.portrayed > b.portrayed ? -1 : 0;
+    };
+
+    const searchOnTextFieldChange = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setSearchValue(e.target.value);
+    };
+
+    const sortOnSelectChange = (e: { target: { value: string; } }) => {
+        setDropDownValue(e.target.value as string);
     };
 
     useEffect(() => {
@@ -46,14 +49,14 @@ export default function App () {
     }, [searchValue, dispatch]);
 
     useEffect(() => {
-        if (dropDownValue === "1" && userValue) {
-            setUserValue([...userValue].sort(characterAscending));
-        } else if (dropDownValue === "2" && userValue) {
-            setUserValue([...userValue].sort(characterDesscending));
-        } else if (dropDownValue === "3" && userValue) {
-            setUserValue([...userValue].sort(actorAscending));
-        } else if (dropDownValue === "4" && userValue) {
-            setUserValue([...userValue].sort(actorDesscending));
+        if (dropDownValue === "1") {
+            setUserValue([...user].sort(characterAscending));
+        } else if (dropDownValue === "2") {
+            setUserValue([...user].sort(characterDescending));
+        } else if (dropDownValue === "3") {
+            setUserValue([...user].sort(actorAscending));
+        } else if (dropDownValue === "4") {
+            setUserValue([...user].sort(actorDescending));
         }
     }, [dropDownValue]);
 
@@ -66,9 +69,7 @@ export default function App () {
                         label="Search"
                         variant="outlined"
                         value={ searchValue }
-                        onChange={ (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-                            setSearchValue(e.target.value);
-                        } }
+                        onChange={ searchOnTextFieldChange }
                     />
                     <Box className="drop-down">
                         <FormControl fullWidth>
@@ -79,9 +80,7 @@ export default function App () {
                                 id="demo-simple-select"
                                 value={ dropDownValue }
                                 label="Outlined"
-                                onChange={ (e) => {
-                                    setDropDownValue(e.target.value as string);
-                                } }
+                                onChange={ sortOnSelectChange }
                             >
                                 <MenuItem value={ "1" }>Character name ascending</MenuItem>
                                 <MenuItem value={ "2" }>Character name descending</MenuItem>
@@ -94,20 +93,18 @@ export default function App () {
                 { isError ? message : "" }
                 { userValue?.length === 0 ? "no actors" : "" }
                 <Grid container spacing={ 4 }>
-                    { isSuccess && userValue && userValue.map((user: any) => {
-                        return (
-                            <Grid item xs={ 3 } key={ user.char_id }>
-                                <Box className="user">
-                                    <Box className="img">
-                                        <img src={ user.img } alt={ user.name }/>
-                                    </Box>
-                                    <article>
-                                        <p>{ user.name }<span>{ user.nickname }</span></p>
-                                    </article>
+                    { isSuccess && userValue.map((user: any) => (
+                        <Grid item xs={ 3 } key={ user.char_id }>
+                            <Box className="user">
+                                <Box className="img">
+                                    <img src={ user.img } alt={ user.name }/>
                                 </Box>
-                            </Grid>
-                        );
-                    }) }
+                                <article>
+                                    <p>{ user.name }<span>{ user.nickname }</span></p>
+                                </article>
+                            </Box>
+                        </Grid>
+                    )) }
                 </Grid>
             </Box>
         </>
--- src/reducer/search.ts
+++ src/reducer/search.ts
@@ -1,16 +1,8 @@
 import axios from "axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export interface IInitialState {
-    user: null | any[];
-    isError: boolean;
-    isSuccess: boolean;
-    isLoading: boolean;
-    message: string;
-}
-
-const initialState: IInitialState = {
-    user: null,
+const initialState = {
+    user: [],
     isError: false,
     isSuccess: false,
     isLoading: false,
@@ -34,22 +26,19 @@ export const searchSlice = createSlice({
     reducers: {},
 
     extraReducers: (builder) => {
-        builder
-            .addCase(search.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(search.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.isSuccess = true;
-                state.user = action.payload;
-            })
-            .addCase(search.rejected, (state, action) => {
-                state.isLoading = false;
-                state.isSuccess = false;
-                state.isError = true;
-                state.message = action.payload;
-                state.user = null;
-            });
+        builder.addCase(search.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(search.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.user = action.payload;
+        }).addCase(search.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = false;
+            state.isError = true;
+            state.message = action.payload;
+            state.user = [];
+        });
     },
 });
 
